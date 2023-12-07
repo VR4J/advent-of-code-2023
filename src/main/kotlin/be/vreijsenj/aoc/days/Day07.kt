2c879632e61c851184e2bc08fa18a6ce953e8514
Joery Vreijsen <joery_vreijsen@hotmail.com>
2023-12-07 17:01:22 +0100

    Clean up Day 7, fix tests day 5

--- src/main/kotlin/be/vreijsenj/aoc/days/Day07.kt
+++ src/main/kotlin/be/vreijsenj/aoc/days/Day07.kt
@@ -4,105 +4,92 @@ import be.vreijsenj.aoc.utils.PuzzleUtils
 import java.lang.RuntimeException
 import kotlin.time.measureTime
 
-enum class Type(val strength: Int, val isPresent: (occurrences: Map<Int, Int>, jokers: Int) -> Boolean) {
+enum class ResultType(val strength: Int, val isPresent: (occurrences: Map<Int, Int>, jokers: Int) -> Boolean) {
     FIVE_OF_A_KIND(7, { occurrences, jokers ->
-        if(jokers >= 4) {
-            true
-        } else {
-            occurrences.any { it.value == (5 - jokers) }
+        when(jokers) {
+            0, 1, 2, 3 -> occurrences.any { it.value == (5 - jokers) }
+            else -> true
         }
     }),
     FOUR_OF_A_KIND(6, { occurrences, jokers ->
-        if(jokers >= 3) {
-            true
-        } else {
-            occurrences.any { it.value == (4 - jokers) }
+        when(jokers) {
+            0, 1, 2 -> occurrences.any { it.value == (4 - jokers) }
+            else -> true
         }
     }),
     FULL_HOUSE(5, { occurrences, jokers ->
-        if(jokers == 0) {
-            occurrences.containsValue(3) && occurrences.containsValue(2)
-        } else if(jokers == 1) {
-            occurrences.count { it.value == 2 } == 2
-                || (occurrences.count { it.value == 3 } == 1 && occurrences.count { it.value == 1 } == 1)
-        } else if(jokers == 2) {
-            occurrences.count { it.value == 2 } == 1
-                && occurrences.count {it.value == 1 } == 1
-        } else {
-            // with three or more jokers you will always have four of a kind.
-            false
+        when (jokers) {
+            0 -> occurrences.containsValue(3) && occurrences.containsValue(2)
+            1 -> {
+                occurrences.count { it.value == 2 } == 2
+                        || (occurrences.count { it.value == 3 } == 1 && occurrences.count { it.value == 1 } == 1)
+            }
+            2 -> {
+                occurrences.count { it.value == 2 } == 1
+                        && occurrences.count {it.value == 1 } == 1
+            }
+            else -> {
+                // with three or more jokers you will always have four of a kind, which is higher
+                false
+            }
         }
     }),
     THREE_OF_A_KIND(4, { occurrences, jokers ->
-        if(jokers >= 2) {
-            true
-        } else {
-            occurrences.any { it.value == (3 - jokers) }
+        when(jokers) {
+            0, 1 -> occurrences.any { it.value == (3 - jokers) }
+            else -> true
         }
     }),
     TWO_PAIRS(3, { occurrences, jokers ->
-        if(jokers == 0) {
-            occurrences.count { it.value == 2 } == 2
-        } else {
-            // with two or more jokers you will always have three of a kind 
-            false
+        when(jokers) {
+            0 -> occurrences.count { it.value == 2 } == 2
+            else -> {
+                // With one or more jokers you will already have three of a kind, which is higher
+                false
+            }
         }
     }),
     ONE_PAIR(2, { occurrences, jokers ->
-        if(jokers == 0) {
-            occurrences.count { it.value == 2 } == 1
-        } else { 
-            true
+        when(jokers) {
+            0 -> occurrences.count { it.value == 2 } == 1
+            1 -> true
+            else -> {
+                // With two or more jokers you will always have three of a kind, which is higher
+                false
+            }
         }
     }),
-    HIGHEST_CARD(1, { occurrences, jokers ->
-        if(jokers > 0) {
-            false
-        } else {
-            true
-        }
-    })
+    HIGHEST_CARD(1, { _,_ -> true })
 }
 
 data class Hand(
     val cards: List<CamelCard>,
+    val jokers: List<CamelCard> = cards.filter { it.value == 1 },
+    var result: ResultType = ResultType.HIGHEST_CARD,
     val bid: Int
 ): Comparable<Hand> {
     companion object {
         @JvmStatic
-        fun parse(line: String): Hand {
+        fun parse(line: String, getCardValue: (String) -> Int): Hand {
             val (cards, bid) = line.split(" ")
 
             return Hand(
-                cards = cards.chunked(1).map { CamelCard.parse(it) },
+                cards = cards.chunked(1).map { CamelCard.parse(it, getCardValue) },
                 bid = bid.toInt()
             )
         }
     }
 
-    fun result(): Int {
-        val group = cards.groupingBy { it.value }
-                         .eachCount()
-
-        val occurrences = group.filter { it.key != 1 }
-        val jokers = group.getOrElse(1) { 0 }
-        
-        val result = Type.values()
-                   .sortedByDescending { it.strength }
-                   .filter { it.isPresent(occurrences, jokers) }
-                   .first
-        
-        print(result)
-
-        return result.strength
+    init {
+        result = result()
     }
 
     override fun compareTo(other: Hand): Int {
-        if(this.result() > other.result()) {
+        if(this.result.strength > other.result.strength) {
             return 1
         }
 
-        if(this.result() == other.result()) {
+        if(this.result.strength == other.result.strength) {
             val pair = this.cards.zip(other.cards)
                 .filter { it.first != it.second }
                 .first
@@ -114,36 +101,34 @@ data class Hand(
             if(pair.first.value < pair.second.value) {
                 return -1
             }
-
-            throw RuntimeException("Unable to compare Hand results")
         }
 
-        if(this.result() < other.result()) {
+        if(this.result.strength < other.result.strength) {
             return -1
         }
 
-        throw RuntimeException("Unable to compare Hand results")
+        throw RuntimeException("Unable to determine winning hand.")
+    }
+
+    private fun result(): ResultType {
+        val grouped = cards
+            .filter { it.value > 1 }
+            .groupingBy { it.value }
+            .eachCount()
+
+        return ResultType.entries.toTypedArray()
+            .filter { it.isPresent(grouped, jokers.size) }
+            .first
     }
 }
 data class CamelCard(val name: String, val value: Int) {
     companion object {
         @JvmStatic
-        fun parse(name: String): CamelCard {
+        fun parse(name: String, getCardValue: (String) -> Int): CamelCard {
             return CamelCard(
                 name = name,
-                value = getValue(name),
+                value = getCardValue(name),
             )
-        }
-
-        private fun getValue(name: String): Int {
-            return when(name) {
-                "A" -> 14
-                "K" -> 13
-                "Q" -> 12
-                "T" -> 10
-                "J" -> 1
-                else -> name.toInt()
-            }
         }
     }
 }
@@ -152,18 +137,51 @@ object Day07 {
     @JvmStatic
     fun main(args: Array<String>) {
         val elapsed = measureTime {
-            val inputPartOne = PuzzleUtils.getInput(7, 1)
-            val resultPartOne = runPartOne(inputPartOne)
+            val input = PuzzleUtils.getInput(7, 1)
 
-            println("Ways to beat the records (pt.1): $resultPartOne")
+            val resultPartOne = runPartOne(input)
+            val resultPartTwo = runPartTwo(input)
+
+            println("Total winnings (pt.1): $resultPartOne")
+            println("Total winnings (pt.2): $resultPartTwo")
         }
 
         println("Took: $elapsed")
     }
 
     fun runPartOne(input: List<String>): Int {
+        val getCardValue = { name: String ->
+            when(name) {
+                "A" -> 14
+                "K" -> 13
+                "Q" -> 12
+                "J" -> 11
+                "T" -> 10
+                else -> name.toInt()
+            }
+        }
+
+        return run(input, getCardValue)
+    }
+
+    fun runPartTwo(input: List<String>): Int {
+        val getCardValue = { name: String ->
+            when(name) {
+                "A" -> 14
+                "K" -> 13
+                "Q" -> 12
+                "T" -> 10
+                "J" -> 1
+                else -> name.toInt()
+            }
+        }
+
+        return run(input, getCardValue)
+    }
+
+    private fun run(input: List<String>, getCardValue: (String) -> Int): Int {
         return input
-            .map { Hand.parse(it) }
+            .map { Hand.parse(it, getCardValue) }
             .sorted()
             .mapIndexed { index, hand ->
                 hand.bid * (index + 1)
--- src/test/groovy/be/vreijsenj/aoc/days/Day07Test.groovy
+++ src/test/groovy/be/vreijsenj/aoc/days/Day07Test.groovy
@@ -1,12 +1,10 @@
 package be.vreijsenj.aoc.days
 
-import be.vreijsenj.aoc.utils.PuzzleUtils
-
 import spock.lang.Specification
 
 class Day07Test extends Specification {
 
-     def "returns example total winnings of all hands considering jokers"() {
+     def "returns total winnings of all hands"() {
         given: "the camel card hands"
             def input = [
                 "32T3K 765",
@@ -20,39 +18,63 @@ class Day07Test extends Specification {
             def result = new Day07().runPartOne(input)
 
         then: "the result matches the example answer"
-            result == 5905
+            result == 6440
     }
     
     def "returns total winnings of all hands considering jokers"() {
         given: "the camel card hands"
-            def input = PuzzleUtils.getInput(7, 1)
+            def input = [
+                    "32T3K 765",
+                    "T55J5 684",
+                    "KK677 28",
+                    "KTJJT 220",
+                    "QQQJA 483"
+            ]
 
         when: "the games are played"
-            def result = new Day07().runPartOne(input)
+            def result = new Day07().runPartTwo(input)
 
         then: "the result matches the example answer"
             result == 5905
     }
 
-     def "returns the types for different hands"() {
-          given: "the camel card hands"
-               def input = [
-                    "JJ333 100",
-                    "J3332 100",
-                    "J3322 100",
-                    "JJ245 100",
-                    "22445 100",
-                    "J2589 100",
-                    "34781 100"
-               ]
+    def "returns the types for different hands"() {
+        given: "the camel card hands"
+            def input = [
+                "JJ333 100",
+                "J3332 100",
+                "J3322 100",
+                "JJ245 100",
+                "22445 100",
+                "J2589 100",
+                "34782 100"
+            ]
 
-          when: "the games are played"
-               def result = input
-                    .collect { Hand.parse(it) }
-                    .collect { it.result() }
+        when: "the games are played"
+            def getCardValue = { name ->
+                return switch (name) {
+                    case "A" -> 14
+                    case "K" -> 13
+                    case "Q" -> 12
+                    case "T" -> 10
+                    case "J" -> 1
+                    default -> name.toInteger()
+                }
+            }
 
-          then: "the result"
-               result == []
-               
+            def result = input
+                    .collect { Hand.parse(it, getCardValue) }
+                    .collect { it.result }
+
+        then: "the result"
+            result == [
+                ResultType.FIVE_OF_A_KIND,
+                ResultType.FOUR_OF_A_KIND,
+                ResultType.FULL_HOUSE,
+                ResultType.THREE_OF_A_KIND,
+                ResultType.TWO_PAIRS,
+                ResultType.ONE_PAIR,
+                ResultType.HIGHEST_CARD
+            ]
      }
 }
